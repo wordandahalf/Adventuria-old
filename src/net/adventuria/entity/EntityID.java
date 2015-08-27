157c2b00b5d7b233e58be1ca00e8dcdca7466436
Ryan J. <mail2ryanjones@gmail.com>
2015-08-26 22:16:47 -0500

    Added more entity types
    NOTE: Please comply with MY coding style; if you do not, I will not allow you to commit any changes!

--- src/net/adventuria/entity/EntityID.java
+++ src/net/adventuria/entity/EntityID.java
@@ -1,7 +1,18 @@
 package net.adventuria.entity;
 
-public enum EntityID {
-	NULL(0, new int[] { -1, -1 }, ""), ITEM(1, new int[] { 0, 0 }, "Generic Item"), PIG(10, new int[] { 0, 0 }, 10, "Pig"), COW(11, new int[] { 0, 0 }, 10, "Cow"), SHEEP(12, new int[] { 0, 0 }, 10, "Sheep"), CHICKEN(13, new int[] { 0, 0 }, 5, "Chicken"), PLAYER(255, new int[] { 0, 18 }, 20, "Player");
+public enum EntityID
+{
+	NULL(0, new int[] { -1, -1 }, ""),
+	ITEM(1, new int[] { 0, 0 }, "Generic Item"),
+	PIG(10, new int[] { 0, 0 }, 10, "Pig"),
+	COW(11, new int[] { 0, 0 }, 10, "Cow"),
+	SHEEP(12, new int[] { 0, 0 }, 10, "Sheep"),
+	CHICKEN(13, new int[] { 0, 0 }, 5, "Chicken"),
+	ZOMBIE(20, new int[] {0, }, 20, "Zombie"),
+	SKELETON(21, new int[] {0, 0}, 15, "Skeleton"),
+	SPIDER(22, new int[] {0, 0}, 15, "Spider"),
+	GHOST(23, new int[] {0, 0}, 15, "Ghost"),
+	PLAYER(255, new int[] { 0, 18 }, 20, "Player");
 
 	private int ID = 0;
 	private int defaultHealth = 0;
@@ -10,32 +21,39 @@ public enum EntityID {
 
 	private String name = "";
 
-	private EntityID(int ID, int[] textureID, String name) {
+	private EntityID(int ID, int[] textureID, String name)
+	{
 		this.ID = ID;
 		this.textureID = textureID;
 		this.name = name;
+		this.defaultHealth = -1;
 	}
 
-	private EntityID(int ID, int[] textureID, int defaultHealth, String name) {
+	private EntityID(int ID, int[] textureID, int defaultHealth, String name)
+	{
 		this.ID = ID;
 		this.textureID = textureID;
 		this.name = name;
 		this.defaultHealth = defaultHealth;
 	}
 
-	public int getID() {
+	public int getID()
+	{
 		return this.ID;
 	}
 
-	public int[] getTextureID() {
+	public int[] getTextureID()
+	{
 		return this.textureID;
 	}
 
-	public String getName() {
+	public String getName()
+	{
 		return this.name;
 	}
 
-	public int getDefaultHealth() {
+	public int getDefaultHealth()
+	{
 		return this.defaultHealth;
 	}
 }
