100a1f49c61e4a6943cd96b71feb874f71ecc25a
redusk <hayden.folker@gmail.com>
2016-04-24 21:28:52 -0600

    Texture Updates
    Updated textures to make them look better,
    modified Biome constructor to include sky color to allow  sky to change
    based on biome

--- src/net/adventuria/level/biome/Biome.java
+++ src/net/adventuria/level/biome/Biome.java
@@ -1,22 +1,29 @@
 package net.adventuria.level.biome;
 
 import java.util.Random;
+import java.awt.Color;
 
 public enum Biome {
-	PLAINS(0, "Plains"),
-	DESERT(1, "Desert"),
-	OCEAN(2, "Ocean");
+	PLAINS(0, "Plains", new Color(70, 120, 230)),
+	DESERT(1, "Desert", new Color(70, 120, 230)),
+	OCEAN(2, "Ocean", new Color(70, 120, 230));
 	
 	private int id;
 	private String name;
+	private Color sky;
 	
-	private Biome(int id, String name) {
+	private Biome(int id, String name, Color skyColor) {
 		this.id = id;
 		this.name = name;
+		this.sky = skyColor;
 	}
 	
 	public int getID() {
 		return this.id;
+	}
+	
+	public Color getSkyColor() {
+		return sky;
 	}
 	
 	public String getName() {
